239e40287e86cc0cb5a9967991230a5a1d09e821
showzyl <showzyl520@gmail.com>
2016-02-17 10:07:09 +0800

    d

--- array.js
+++ array.js
@@ -3,7 +3,97 @@
  */
 'use strict';
 
-let a = 123;
-console.log(a);
+// 实现队列
+
+//let aQueue = [];
+//
+////unshift: 在数组的开头添加一个或更多元素，并返回新的长度
+//
+//console.log('入队');
+//
+//aQueue.unshift(1);
+//
+//console.log(aQueue);
+//
+//aQueue.unshift(2);
+//
+//console.log(aQueue);
+//
+//aQueue.unshift(3);
+//
+//console.log(aQueue);
+//
+//aQueue.unshift(4);
+//
+//console.log(aQueue);
+//
+//console.log("出队，先进先出");
+//
+////pop：从数组中把最后一个元素删除，并返回这个元素的值
+//
+//aQueue.pop();
+//
+//console.log(aQueue);
+//
+//aQueue.pop();
+//
+//console.log(aQueue);
+//
+//aQueue.pop();
+//
+//console.log(aQueue);
+//
+//aQueue.pop();
+//
+//console.log(aQueue);
+
+
+// 实现堆栈
+
+var aStack = [];
+
+console.log('入栈')
+
+aStack.push(1);
+
+console.log(aStack);
+
+aStack.push(2);
+
+console.log(aStack);
+
+aStack.push(3);
+
+console.log(aStack);
+
+aStack.push(4);
+
+console.log(aStack);
+
+aStack.pop();
+
+console.log(aStack);
+
+aStack.pop();
+
+console.log(aStack);
+
+aStack.pop();
+
+console.log(aStack);
+
+aStack.pop();
+
+console.log(aStack);
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ demo.js
@@ -0,0 +1,32 @@
+/**
+ * Created by showzyl on 16/2/17.
+ */
+'use strict';
+
+const sequencify = require('./sequencify');
+
+let items = {
+  a: {
+    name: 'a',
+    dep: []
+    // other properties as needed
+  },
+  b: {
+    name: 'b',
+    dep: ['a']
+  },
+  c: {
+    name: 'c',
+    dep: ['a']
+  },
+  d: {
+    name: 'd',
+    dep: ['c']
+  }
+};
+
+let names = ['d', 'b', 'c', 'a', 'e']; // The names of the items you want arranged, need not be all
+
+let results = sequencify(items, names);
+
+console.log(results);
